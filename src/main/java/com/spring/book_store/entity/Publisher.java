7fc2b617c09717f69efb956798ed431b5e9e7643
asou safari <asousafari1990@gmail.com>
2024-05-23 21:30:01 +0330

    add Publisher Entity

---
+++ src/main/java/com/spring/book_store/entity/Publisher.java
@@ -0,0 +1,43 @@
+package com.spring.book_store.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+/**
+ * Author: ASOU SAFARI
+ * Date:5/23/24
+ * Time:9:01 PM
+ */
+
+@Setter
+@Getter
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Entity
+public class Publisher {
+
+    @Id
+    @UuidGenerator
+    @Column(length = 36, columnDefinition = "varchar",updatable = false,nullable = false)
+    private UUID id;
+    private String label;
+    private String ZipCode;
+    @Version
+    private Integer version;
+
+    @OneToMany(mappedBy = "publisher")
+    private Set<Book> books = new HashSet<>();
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Author> authors = new HashSet<>();
+
+
+}
