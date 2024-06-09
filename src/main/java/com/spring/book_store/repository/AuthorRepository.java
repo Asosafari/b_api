12765e1e27d320a99f676220d6f491a0bc3bcc4f
asou safari <asousafari1990@gmail.com>
2024-06-09 21:09:22 +0330

    implement find Author by name,lastName

--- src/main/java/com/spring/book_store/repository/AuthorRepository.java
+++ src/main/java/com/spring/book_store/repository/AuthorRepository.java
@@ -1,6 +1,8 @@
 package com.spring.book_store.repository;
 
 import com.spring.book_store.entity.Author;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
@@ -11,4 +13,7 @@ import java.util.UUID;
  * Time:10:49 PM
  */
 public interface AuthorRepository extends JpaRepository<Author,UUID> {
+    Page<Author> findAllByNameIsLikeIgnoreCase(String name, Pageable pageable);
+    Page<Author> findAllByLastNameLikeIgnoreCase(String lastName,Pageable pageable);
+    Page<Author> findAllByNameIsLikeIgnoreCaseAndLastNameLikeIgnoreCase(String name, String lastName, Pageable pageable);
 }
