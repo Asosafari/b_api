8cc897a01f187fae96ce501c004fbde1a33b8aa8
asou safari <asousafari1990@gmail.com>
2024-06-09 21:10:06 +0330

    implement find Book by title

--- src/main/java/com/spring/book_store/repository/BookRepository.java
+++ src/main/java/com/spring/book_store/repository/BookRepository.java
@@ -1,9 +1,13 @@
 package com.spring.book_store.repository;
 
 import com.spring.book_store.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface BookRepository extends JpaRepository<Book,UUID> {
+
+    Page<Book> findAllBytitleIsLikeIgnoreCase(String title, Pageable pageable);
 }
