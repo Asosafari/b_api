2530c21377709588d557e538b05414c08e466fe1
asou safari <asousafari1990@gmail.com>
2024-06-09 21:10:31 +0330

    implement find Book by label

--- src/main/java/com/spring/book_store/repository/PublisherRepository.java
+++ src/main/java/com/spring/book_store/repository/PublisherRepository.java
@@ -1,9 +1,13 @@
 package com.spring.book_store.repository;
 
 import com.spring.book_store.entity.Publisher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface PublisherRepository extends JpaRepository<Publisher,UUID> {
+
+    Page<Publisher> findAllByLabelIsLikeIgnoreCase(String label, Pageable pageable);
 }
