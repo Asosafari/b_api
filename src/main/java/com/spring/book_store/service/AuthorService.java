7a1baf4f0d0587492e6c2c3b1391d0daca1116c0
asou safari <asousafari1990@gmail.com>
2024-06-09 23:08:39 +0330

    Create AuthorService interface

---
+++ src/main/java/com/spring/book_store/service/AuthorService.java
@@ -0,0 +1,19 @@
+package com.spring.book_store.service;
+import com.spring.book_store.model.AuthorDTO;
+import org.springframework.data.domain.Page;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface AuthorService {
+    Page<AuthorDTO> listOfAuthors(String name, String lastName, Integer pageNumber,Integer pageSize);
+    Optional<AuthorDTO> getAuthorById(UUID id);
+    AuthorDTO saveNewAuthor(AuthorDTO author);
+    Optional<AuthorDTO> updateAuthorById(UUID id,AuthorDTO authorDTO);
+    boolean deleteAuthoeById(UUID id);
+    Optional<AuthorDTO> patchById(UUID id);
+
+
+}
