fb93e4c4c8259bbc7da2fad48bbc44da4ca28177
asou safari <asousafari1990@gmail.com>
2024-06-10 19:28:50 +0330

    Implementation AuthorService interface find Author with book title and publisher label and create test

--- src/test/java/com/spring/book_store/repository/AuthorRepositoryTest.java
+++ src/test/java/com/spring/book_store/repository/AuthorRepositoryTest.java
@@ -1,12 +1,16 @@
 package com.spring.book_store.repository;
 
 import com.spring.book_store.entity.Author;
+import com.spring.book_store.entity.Book;
+import com.spring.book_store.entity.Publisher;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -30,17 +34,18 @@ class AuthorRepositoryTest {
                 .createdDate(LocalDateTime.now())
                 .updateDate(LocalDateTime.now())
                 .build());
+
     }
 
     @Test
-    void tesGetBeerListByName() {
+    void tesGetAuthorListByName() {
         Page<Author> page = authorRepository.findAllByNameIsLikeIgnoreCase("%ali%", null);
         assertThat(page.getContent().size()).isEqualTo(1);
         assertThat(page.getContent().get(0)).isEqualTo(saveAuthor);
     }
 
     @Test
-    void tesGetBeerListByLastName() {
+    void tesGetAuthorListByLastName() {
         Page<Author> page = authorRepository
                 .findAllByLastNameLikeIgnoreCase("rad", PageRequest.of(0, 10));
         assertThat(page.getContent().size()).isEqualTo(1);
@@ -48,7 +53,7 @@ class AuthorRepositoryTest {
     }
 
     @Test
-    void tesGetBeerListByFullName() {
+    void tesGetAuthorListByFullName() {
         Page<Author> page = authorRepository
                 .findAllByNameIsLikeIgnoreCaseAndLastNameLikeIgnoreCase("ali" ,"rad",
                         PageRequest.of(0, 10));
