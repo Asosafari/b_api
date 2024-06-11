e57411a42088311cf492f9110fdb402969a6381d
asou safari <asousafari1990@gmail.com>
2024-06-11 16:44:19 +0330

    Edit AuthorService interface

--- src/main/java/com/spring/book_store/service/BookService.java
+++ src/main/java/com/spring/book_store/service/BookService.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface BookService {
-    Page<Optional<BookDTO>> listOfBooks(String title, String authorName, String authorLastName,
+    Page<BookDTO> listOfBooks(String title, String authorName, String authorLastName,
                                         String publisherLabel, Integer pageNumber, Integer pageSize);
     Optional<BookDTO> getBookById(UUID id);
     BookDTO saveNewBook(BookDTO bookDTO);
