242972ecae8c30e9f729df2008a338d454d96ea8
asou safari <asousafari1990@gmail.com>
2024-06-11 20:53:39 +0330

    Create PublisherService and PublisherServiceImplJPA

---
+++ src/main/java/com/spring/book_store/service/PublisherService.java
@@ -0,0 +1,18 @@
+package com.spring.book_store.service;
+
+import com.spring.book_store.model.PublishetDTO;
+import org.springframework.data.domain.Page;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface PublisherService {
+    Page<PublishetDTO> listOfPublisher(String label, String bookTitle, String authorFullName);
+    Optional<PublishetDTO> getPublisherById(UUID id);
+    PublishetDTO saveNewPublisher (PublishetDTO publishetDTO);
+    Optional<PublishetDTO> updatePublisherById(UUID id, PublishetDTO publishetDTO);
+    Optional<PublishetDTO> patchPublisherById(UUID id, PublishetDTO publishetDTO);
+    boolean deletePublisherById(UUID id);
+
+
+}
